33c87e798de565d0a42d59b11f8998bcd21f844a
claes <claesca@outlook.com>
2017-04-25 19:02:06 +0200

    add footer as well as working logo

--- spec/features/test_index_spec.rb
+++ spec/features/test_index_spec.rb
@@ -26,5 +26,13 @@ describe 'index', type: :feature do
     end
   end
 
-  
+  it 'renders footer partial' do
+    expect(page).to have_selector 'footer'
+    within 'footer' do
+      expect(page).to have_content 'My Portfolio'
+      expect(page).to have_content 'Built using the Middleman framework'
+    end
+  end
+
+
 end
