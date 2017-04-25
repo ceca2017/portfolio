3ae357c2adfcf4eb1e3bdc3d06ce9f1525d2219f
claes <claesca@outlook.com>
2017-04-25 18:40:28 +0200

    add yml db and test

--- spec/features/projects_spec.rb
+++ spec/features/projects_spec.rb
@@ -3,4 +3,12 @@ require 'spec_helper'
 
 describe 'projects', type: :feature do
 
+  before do
+    visit '/projects'
+  end
+
+  it 'does something' do
+
+  end
+
 end
--- spec/features/test_index_spec.rb
+++ spec/features/test_index_spec.rb
@@ -1,6 +1,7 @@
 require 'spec_helper'
 
 describe 'index', type: :feature do
+
   before do
     visit '/'
   end
@@ -16,4 +17,14 @@ describe 'index', type: :feature do
   it 'displays Middleman logo' do
     expect(page).to have_css 'img[src*="middleman-logo.svg"]'
   end
+
+  it 'displays project list' do
+    expect(page).to have_css '.projects'
+    within '.projects' do
+      expect(page).to have_content 'My First Website'
+      expect(page).to have_content 'FizzBuzz'
+    end
+  end
+
+  
 end
