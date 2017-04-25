7684a0f53340b1b84194b5c13d252708605c4187
claes <claesca@outlook.com>
2017-04-25 14:24:17 +0200

    add of sample test via Thomas video

---
+++ spec/features/index_spec.rb
@@ -0,0 +1,19 @@
+require 'spec_helper'
+
+describe 'index', type: :feature do
+
+  before  do
+    visit '/'
+  end
+
+  it 'says Middleman is fun' do
+    expect(page).to have_selector 'h1'
+    within 'h1' do
+      expect(page).to have_content 'Middleman is fun'
+    end
+  end
+
+  it 'displays Middleman logo' do
+    expect(page).to have_css 'img[src*="middleman-logo.svg"]'
+  end
+end
